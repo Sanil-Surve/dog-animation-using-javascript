7d9922b78b4296547c5e6ed9f984db41a9d1459a
Sanil-Surve <sanilsurve42@gmail.com>
2024-07-12 12:15:27 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,87 @@
+import React, { useRef, useEffect, useCallback, useState } from 'react';
+import './App.css';
+import dog from '/shadow_dog.png';
+
+const App = () => {
+  const canvasRef = useRef(null);
+  const requestRef = useRef();
+  const [speed, setSpeed] = useState(5); // default speed for walking
+  const [isAnimating, setIsAnimating] = useState(true);
+
+  const draw = useCallback((ctx, frameX, frameY, spriteWidth, spriteHeight, playerImage) => {
+    ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+    ctx.drawImage(
+      playerImage,
+      frameX * spriteWidth,
+      frameY * spriteHeight,
+      spriteWidth,
+      spriteHeight,
+      0,
+      0,
+      spriteWidth,
+      spriteHeight
+    );
+  }, []);
+
+  useEffect(() => {
+    const canvas = canvasRef.current;
+    const ctx = canvas.getContext('2d');
+
+    const CANVAS_WIDTH = (canvas.width = 600);
+    const CANVAS_HEIGHT = (canvas.height = 600);
+
+    const playerImage = new Image();
+    playerImage.src = dog;
+    const spriteWidth = 575;
+    const spriteHeight = 523;
+
+    let frameX = 1;
+    let frameY = 3;
+
+    let gameFrame = 0;
+
+    const animate = () => {
+      if (!isAnimating) return;
+
+      draw(ctx, frameX, frameY, spriteWidth, spriteHeight, playerImage);
+      if (gameFrame % speed === 0) {
+        frameX = frameX < 7 ? frameX + 1 : 0;
+      }
+      gameFrame++;
+      requestRef.current = requestAnimationFrame(animate);
+    };
+
+    playerImage.onload = () => {
+      animate();
+    };
+
+    return () => {
+      cancelAnimationFrame(requestRef.current);
+    };
+  }, [draw, speed, isAnimating]);
+
+  const handleStop = () => setIsAnimating(false);
+  const handleWalk = () => {
+    setIsAnimating(true);
+    setSpeed(5);
+  };
+  const handleRun = () => {
+    setIsAnimating(true);
+    setSpeed(1);
+  };
+
+  return (
+    <>
+      <header>Dog Animation</header>
+      <canvas id="canvas1" ref={canvasRef}></canvas>
+      <footer>DogSprite Animator</footer>
+      <div className="controls">
+        <button onClick={handleStop}>Stop</button>
+        <button onClick={handleWalk}>Walk</button>
+        <button onClick={handleRun}>Run</button>
+      </div>
+    </>
+  );
+};
+
+export default App;
