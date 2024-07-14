5c9d62ac4c45a00318743ed4836f458f8f73332f
Sanil-Surve <sanilsurve42@gmail.com>
2024-07-14 10:12:50 +0530

    file Changed

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,37 @@
-import React, { useRef, useEffect, useCallback, useState } from 'react';
-import './App.css';
-import dog from '/shadow_dog.png';
+import React, { useRef, useEffect, useCallback, useState } from "react";
+import "./App.css";
+import dog from "/shadow_dog.png";
 
 const App = () => {
   const canvasRef = useRef(null);
   const requestRef = useRef();
-  const [speed, setSpeed] = useState(5); // default speed for walking
+  const [speed, setSpeed] = useState(5); 
   const [isAnimating, setIsAnimating] = useState(true);
 
-  const draw = useCallback((ctx, frameX, frameY, spriteWidth, spriteHeight, playerImage) => {
-    ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
-    ctx.drawImage(
-      playerImage,
-      frameX * spriteWidth,
-      frameY * spriteHeight,
-      spriteWidth,
-      spriteHeight,
-      0,
-      0,
-      spriteWidth,
-      spriteHeight
-    );
-  }, []);
+  const draw = useCallback(
+    (ctx, frameX, frameY, spriteWidth, spriteHeight, playerImage) => {
+      ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+      ctx.drawImage(
+        playerImage,
+        frameX * spriteWidth,
+        frameY * spriteHeight,
+        spriteWidth,
+        spriteHeight,
+        0,
+        0,
+        spriteWidth,
+        spriteHeight
+      );
+    },
+    []
+  );
 
   useEffect(() => {
     const canvas = canvasRef.current;
-    const ctx = canvas.getContext('2d');
+    const ctx = canvas.getContext("2d");
 
-    const CANVAS_WIDTH = (canvas.width = 600);
-    const CANVAS_HEIGHT = (canvas.height = 600);
+    canvas.width = 600;
+    canvas.height = 600;
 
     const playerImage = new Image();
     playerImage.src = dog;
@@ -60,17 +63,17 @@ const App = () => {
     };
   }, [draw, speed, isAnimating]);
 
-  const handleStop = () => {
+  const handleRunSlow = () => {
     setIsAnimating(true);
-    setSpeed(20);
-  }
+    setSpeed(15);
+  };
 
-  const handleWalk = () => {
+  const handleRun = () => {
     setIsAnimating(true);
     setSpeed(5);
   };
 
-  const handleRun = () => {
+  const handleRunFast = () => {
     setIsAnimating(true);
     setSpeed(1);
   };
@@ -80,9 +83,9 @@ const App = () => {
       <header>DogSprite Animator</header>
       <canvas id="canvas1" ref={canvasRef}></canvas>
       <div className="controls">
-        <button onClick={handleStop}>Run Slow</button>
-        <button onClick={handleWalk}>Run</button>
-        <button onClick={handleRun}>Run Fast</button>
+        <button onClick={handleRunSlow}>Run Slow</button>
+        <button onClick={handleRun}>Run</button>
+        <button onClick={handleRunFast}>Run Fast</button>
       </div>
     </>
   );
